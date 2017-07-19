f582ee40efeaaec56adac088558a7f1ecfdd18d8
Louis Tur <louis.tur@gmail.com>
2017-07-19 17:39:20 -0400

    lesson ready

--- Tableviews_Part_1/Actor.swift
+++ Tableviews_Part_1/Actor.swift
@@ -10,28 +10,4 @@ import Foundation
 
 class Actor {
 	
-	var firstName: String
-	var lastName: String
-	
-	init() {
-		self.firstName = ""
-		self.lastName = ""
-	}
-	
-	init(firstName: String, lastName: String) {
-		self.firstName = firstName
-		self.lastName = lastName
-	}
-	
-	convenience init(from string: String) {
-		let nameComponents: [String] = string.components(separatedBy: " ")
-		if let firstName: String = nameComponents.first,
-			let lastName:String = nameComponents.last {
-			self.init(firstName: firstName, lastName: lastName)
-		}
-		else {
-			self.init()
-		}
-		
-	}
 }
--- Tableviews_Part_1/Models/Movie.swift
+++ Tableviews_Part_1/Models/Movie.swift
@@ -9,44 +9,5 @@
 import Foundation
 
 class Movie {
-	
-	var title: String
-	var year: Int
-	var genre: String
-	var cast: [String]
-	var locations: [String]
-	var summary: String
-	
-	init(title: String, year: Int, genre: String, cast: [String], locations: [String], summary: String) {
-		self.title = title
-		self.year = year
-		self.genre = genre
-		self.cast = cast
-		self.locations = locations
-		self.summary = summary
-	}
-	
-	convenience init(from dict: [String : Any]) {
-		if let movieTitle = dict["name"] as? String,
-			let movieYear = dict["year"] as? Int,
-			let movieGenre = dict["genre"] as? String,
-			let movieCast = dict["cast"] as? [String],
-			let movieLocations = dict["locations"] as? [String],
-			let movieSummary = dict["description"] as? String {
-			
-			self.init(title: movieTitle, year: movieYear, genre: movieGenre, cast: movieCast, locations: movieLocations, summary: movieSummary)
-		}
-		else {
-			self.init()
-		}
-	}
-	
-	init() {
-		self.title = ""
-		self.year = 1970
-		self.genre = ""
-		self.cast = []
-		self.locations = []
-		self.summary = ""
-	}
+
 }
